4dc25b31ecd88098380ddd62c8f48602de3e96cf
aminebatsi <101473105+aminebatsi@users.noreply.github.com>
2023-10-05 16:22:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & jenkins
+          Learn React & jenkins CI/CD
         </a>
       </header>
     </div>
